dacc20f63308505122ed9b67583735b94b9b29a2
stephannn <38951729+stephannn@users.noreply.github.com>
2021-03-12 06:54:24 +0100

    Add files via upload

---
+++ ConsoleWinHttpProxySettings/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using hanisch.WinHttpProxySettings;
+
+namespace ConsoleWinHttpProxySettings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            IWinHttpProxySettingsInterface wps = new WinHttpProxySettings();
+            List<IWinHttpProxySettingsInterface.IWinHttpProxyConfig> whpcList = wps.GetWinHttpProxySettings(new ContextObject[] { ContextObject.LocalMachine });
+
+
+            foreach (IWinHttpProxySettingsInterface.IWinHttpProxyConfig whpc in whpcList)
+            {
+                Console.WriteLine("Version: " + whpc.Version);
+                Console.WriteLine("Counter: " + whpc.Counter);
+                Console.WriteLine("ConfigFlags: " + whpc.ConfigFlags);
+                Console.WriteLine("Proxy: " + whpc.Proxy);
+                Console.WriteLine("Bypass: " + whpc.Bypass);
+                Console.WriteLine("AutoConfig: " + whpc.AutoConfig);
+                Console.WriteLine("ProxzSettingPerUser: " + whpc.ProxySettingsPerUser);
+                Console.WriteLine("Context: " + whpc.Context);
+            }
+
+            wps.SetWinHttpProxySettings(ContextObject.LocalMachine, WinHttpFlags.auto, "10.0.0.8");
+
+            #if !DEBUG
+                Console.ReadKey();
+            #endif
+        }
+    }
+}
